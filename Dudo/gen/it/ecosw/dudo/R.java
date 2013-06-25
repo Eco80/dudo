8d84dc5f38571d28a1bbb3d77c135b4b96fbb46a
Enrico Strocchi <strocchienr@gmail.com>
2013-06-25 15:11:19 +0200

    General Update

--- Dudo/gen/it/ecosw/dudo/R.java
+++ Dudo/gen/it/ecosw/dudo/R.java
@@ -103,6 +103,7 @@ public final class R {
         public static final int numplayer_setting_title=0x7f070022;
         public static final int package_not_found=0x7f07001b;
         /**  Backgrounds 
+ Backgrounds 
          */
         public static final int pattern_green_carpet=0x7f07002e;
         /**  Description settings elements 
